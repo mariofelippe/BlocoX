4a5ea96a0acf8f0deb4b691268023ed6109dcd95
Mario Felippe <mfchaves@live.com>
2022-01-11 18:23:03 -0300

    Criação: Primeiros Testes.

---
+++ UnitTestBlocoX/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBlocoX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBlocoX")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07ba6c9f-13ab-4f74-b844-f6fb5468f6cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestBlocoX/Utils/UtilTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BlocoX.Utils;
+
+namespace UnitTestBlocoX
+{
+    [TestClass]
+    [TestCategory("Utils Tests")]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void TestStrToDate()
+        {
+            string str = "2021-01-01";          
+            Assert.AreEqual(Util.StrToDate(str), DateTime.Parse(str));
+        }
+
+        [TestMethod]
+        public void TestStrIntToClear()
+        {
+            string str = "1#f2G?3";
+            Assert.AreEqual(123, Util.StrIntToClear(str));
+        }
+
+        [TestMethod]
+        public void TestStrDecimalToClear()
+        {
+            string valor = "1fçsdf2fa3f4f56789asd";
+            decimal valorDecimal = 1234567.89M;
+            Assert.AreEqual(valorDecimal, Util.StrDecimalToClear(valor)/100);
+        }
+    }
+}
