ab78bcf3067aa91f078ceb8d253996ef4a32f46b
Mario Felippe <mfchaves@live.com>
2022-01-11 16:12:36 -0300

    Criação: Classe Ecf.

---
+++ BlocoX/Models/Ecf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+   public class Ecf
+    {
+        private string numeroFabricacao;
+
+        public Ecf (string numeroFabricacao)
+        {
+            NumeroFabricacao = numeroFabricacao;
+        }
+
+        public string NumeroFabricacao { get => numeroFabricacao; set => numeroFabricacao = value; }
+        
+
+        
+    }
+}
