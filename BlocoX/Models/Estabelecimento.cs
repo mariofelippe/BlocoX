9fbae1a84adc60989b8083ae2e303c7306ebc1e9
Mario Felippe <mfchaves@live.com>
2022-01-11 16:12:13 -0300

    Criação: Classe Estabelecimento.

---
+++ BlocoX/Models/Estabelecimento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class Estabelecimento
+    {
+        private string ie;
+
+
+        // Construtor
+        public Estabelecimento(string inscricaoEstadual)
+        {
+            Ie = inscricaoEstadual;
+        }
+
+
+        public string Ie { get => ie; set => ie = value; }
+    }
+}
