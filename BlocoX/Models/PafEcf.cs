149198c7ca8ff3372c0bba6bd50664d9fb15adc4
Mario Felippe <mfchaves@live.com>
2022-01-11 16:11:36 -0300

    Criação: Classe PafEcf.

---
+++ BlocoX/Models/PafEcf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class PafEcf
+    {
+        private string numeroCredenciamento;
+
+
+        public PafEcf(string numeroCredenciamento)
+        {
+            NumeroCredenciamento = numeroCredenciamento;
+        }
+
+        public string NumeroCredenciamento { get => numeroCredenciamento; set => numeroCredenciamento = value; }
+    }
+}
