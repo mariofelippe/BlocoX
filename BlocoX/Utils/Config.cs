609ca316b8c5dbd872d19f6fe543e4eac7056dfa
Mario Felippe <mfchaves@live.com>
2022-01-11 16:22:31 -0300

    Criação: Classe Config.

---
+++ BlocoX/Utils/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace BlocoX.Utils
+{
+    public class Config
+    {
+        private string credenciamento;
+        private string listaXML;
+        private string pathXML;
+
+        public Config()
+        {
+            CarregaParametrosConfig();  
+
+        }
+
+        public void CarregaParametrosConfig()
+        {          
+            credenciamento = ConfigurationManager.AppSettings.Get("CredenciamentoPaf");
+            listaXML = ConfigurationManager.AppSettings.Get("ListaXML");
+            pathXML = ConfigurationManager.AppSettings.Get("PathXML");
+     
+        }
+
+        public string Credenciamento { get => credenciamento; }
+        public string ListaXML { get => listaXML;  }
+        public string PathXML { get => pathXML; }
+
+
+    }
+}
