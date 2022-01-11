bf53110cbf3959ab59ac5f085d02781f615e6b9d
Mario Felippe <mfchaves@live.com>
2022-01-11 16:15:04 -0300

    Criação: Classe ReducaoZ.

---
+++ BlocoX/Models/ReducaoZ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+   public class ReducaoZ
+    {
+
+        private Ecf ecf;
+        private DadosReducao dadosReducao;
+        private List<TotalizadorParcial> totalizadores;
+
+        public ReducaoZ(Ecf ecf, DadosReducao dadosReducao, List<TotalizadorParcial> totalizadores)
+        {
+            ECF = ecf;
+            DadosReducao = dadosReducao;
+            Totalizadores = totalizadores;
+        }
+
+        public Ecf ECF { get => ecf; set => ecf = value; }
+        public DadosReducao DadosReducao { get => dadosReducao; set => dadosReducao = value; }
+        public List<TotalizadorParcial> Totalizadores { get => totalizadores; set => totalizadores = value; }
+
+    }
+}
