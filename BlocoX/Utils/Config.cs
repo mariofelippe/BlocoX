dc08b82aae0f6af5e47f5f21c7f4d7d0c776a83b
Mario Felippe <mfchaves@live.com>
2022-01-12 22:28:52 -0300

    Ajuste da classe Config.

--- BlocoX/Utils/Config.cs
+++ BlocoX/Utils/Config.cs
@@ -14,7 +14,9 @@ namespace BlocoX.Utils
         private string credenciamento;
         private string listaXML;
         private string pathXML;
-
+        private bool ajustaCredenciamento;
+        private bool ajustaVendaBrutaDiaria;
+        private bool ajustaValorTotalizador;
         public Config()
         {
             CarregaParametrosConfig();  
@@ -22,17 +24,24 @@ namespace BlocoX.Utils
         }
 
         public void CarregaParametrosConfig()
-        {          
+        {
+            ConfigurationManager.RefreshSection("appSettings");
             credenciamento = ConfigurationManager.AppSettings.Get("CredenciamentoPaf");
             listaXML = ConfigurationManager.AppSettings.Get("ListaXML");
             pathXML = ConfigurationManager.AppSettings.Get("PathXML");
-     
+            AjustaCredenciamento = bool.Parse(ConfigurationManager.AppSettings.Get("AjustaCredencimanetoPaf"));
+            AjustaVendaBrutaDiaria = bool.Parse(ConfigurationManager.AppSettings.Get("AjustaVendaBrutaDiaria"));
+            AjustaValorTotalizador = bool.Parse(ConfigurationManager.AppSettings.Get("AjustaValorTotalizaro"));
+            
+
+
         }
 
         public string Credenciamento { get => credenciamento; }
         public string ListaXML { get => listaXML;  }
         public string PathXML { get => pathXML; }
-
-
+        public bool AjustaCredenciamento { get => ajustaCredenciamento; set => ajustaCredenciamento = value; }
+        public bool AjustaVendaBrutaDiaria { get => ajustaVendaBrutaDiaria; set => ajustaVendaBrutaDiaria = value; }
+        public bool AjustaValorTotalizador { get => ajustaValorTotalizador; set => ajustaValorTotalizador = value; }
     }
 }
