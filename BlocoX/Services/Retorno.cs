3bae73c410e2cfb4714812e9c12ddd38cf6167dd
Mario Felippe <mfchaves@live.com>
2022-01-25 12:32:46 -0300

    Crição da classe Retorno e ajuste na ServicoBlocoX.

---
+++ BlocoX/Services/Retorno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Services
+{
+    public class Retorno
+    {
+        private string recibo;
+        private int codigoProcessamento;
+        private string descricao;
+        private string mensagem;
+
+        public string Recibo { get => recibo; set => recibo = value; }
+        public int CodigoProcessamento { get => codigoProcessamento; set => codigoProcessamento = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public string Mensagem { get => mensagem; set => mensagem = value; }
+        
+    }
+}
--- BlocoX/Services/ServicoBlocoX.cs
+++ BlocoX/Services/ServicoBlocoX.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BlocoX.Utils;
+using System.Xml;
 
 namespace BlocoX.Services
 {
@@ -12,14 +13,28 @@ namespace BlocoX.Services
         //private SefazSC.BlocoXSoapClient client = new SefazSC.BlocoXSoapClient();
         private SefazSC.BlocoXSoapClient client = new SefazSC.BlocoXSoapClient();
         private Config config = new Config();
-        public string ConsultarProcessamentoArquivo(string recibo)
+        public Retorno ConsultarProcessamentoArquivo(string recibo)
         {
-           
-            string pXml = Xml.AssinarXML(Xml.XmlConsultarProcessmentoArquivo(recibo),config.GetCertificado());
-            string retorno = client.ConsultarProcessamentoArquivo(pXml);
+            XmlDocument xmlRetorno = new XmlDocument();
+            Retorno retorno = new Retorno();
+            try
+            {
+                string pXml = Xml.AssinarXML(Xml.XmlConsultarProcessmentoArquivo(recibo), config.GetCertificado());
+                string xml = client.ConsultarProcessamentoArquivo(pXml);
+                xmlRetorno.LoadXml(xml);             
+                retorno.Recibo = xmlRetorno.SelectSingleNode("//Recibo").InnerText;
+                retorno.CodigoProcessamento = int.Parse(xmlRetorno.SelectSingleNode("//SituacaoProcessamentoCodigo").InnerText);
+                retorno.Descricao = xmlRetorno.SelectSingleNode("//SituacaoProcessamentoDescricao").InnerText;
+                
+
+            } catch (System.ServiceModel.EndpointNotFoundException e)
+            {
+                Console.WriteLine("Não foi possível fazer a requisição! Verifique o endereço ou a conexão!");
+                
+            }
+
 
             return retorno;
-
         }
 
 
