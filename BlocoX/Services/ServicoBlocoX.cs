057621d5e9a9cb3ec9c8d3b20d080e3fc25f7489
Mario Felippe <mfchaves@live.com>
2022-01-28 22:19:20 -0300

    Criação da função para envio de XML.

--- BlocoX/Services/ServicoBlocoX.cs
+++ BlocoX/Services/ServicoBlocoX.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using BlocoX.Utils;
 using System.Xml;
+using System.IO;
+using System.IO.Compression;
+
 
 namespace BlocoX.Services
 {
     public class ServicoBlocoX
     {
-        //private SefazSC.BlocoXSoapClient client = new SefazSC.BlocoXSoapClient();
+        
         private SefazSC.BlocoXSoapClient client = new SefazSC.BlocoXSoapClient();
         private Config config = new Config();
         public Retorno ConsultarProcessamentoArquivo(string recibo)
@@ -38,6 +41,48 @@ namespace BlocoX.Services
         }
 
 
+        public Retorno EnviaXMLReducaoZ(string xml, string nomeArquivo)
+        {
+            Retorno retorno = new Retorno();
+            XmlDocument xmlRetorno = new XmlDocument();
+            xmlRetorno.LoadXml(client.TransmitirArquivo(Compcatar(xml, nomeArquivo)));
+            retorno.Recibo = xmlRetorno.SelectSingleNode("//Recibo").InnerText;
+            retorno.CodigoProcessamento = int.Parse(xmlRetorno.SelectSingleNode("//SituacaoProcessamentoCodigo").InnerText);
+            retorno.Descricao = xmlRetorno.SelectSingleNode("//SituacaoProcessamentoDescricao").InnerText;
+            retorno.Mensagem = xmlRetorno.SelectSingleNode("//Mensagem").InnerText;
+           
+            
+
+            return retorno;
+        }
+
+        public Byte[] Compcatar(string conteudo, string nomeArquivo)
+        {
+
+            byte[] fileBytes = null;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // Cria o zip
+                using (ZipArchive zip = new ZipArchive(stream, ZipArchiveMode.Create, true))
+                {
+                    // Cria o item a ser adicionado dentro do .zip com o nome especificado.
+                    ZipArchiveEntry entry = zip.CreateEntry(nomeArquivo + ".xml");
+
+                    using (MemoryStream fileStream = new MemoryStream(Encoding.ASCII.GetBytes(conteudo)))
+                    {
+                        //Adiciona os Bytes no ZIp
+                        using (Stream entryStrem = entry.Open())
+                        {
+                            fileStream.CopyTo(entryStrem);
+                        }
+                    }
+                }
+
+                fileBytes = stream.ToArray();
+                
+            }
+            return fileBytes;
+        }
             
     }
 }
