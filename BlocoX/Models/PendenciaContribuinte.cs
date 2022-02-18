20fc86bf31f8f10042ebf4e06b1bf859611129a1
Mario felippe <mfchaves@live.com>
2022-02-18 00:38:38 -0200

    Criação da classe PendenciaContribuinte.

---
+++ BlocoX/Models/PendenciaContribuinte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class PendenciaContribuinte
+    {
+        private Estabelecimento estabelecimento;
+        private DateTime dataInicioObrigacao;
+        private List<PendenciaEcf> pendenciasEcfs;
+
+        public Estabelecimento Estabelecimento { get => estabelecimento; set => estabelecimento = value; }
+        public DateTime DataInicioObrigacao { get => dataInicioObrigacao; set => dataInicioObrigacao = value; }
+        public List<PendenciaEcf> PendenciasEcfs { get => pendenciasEcfs; set => pendenciasEcfs = value; }
+
+        public void AdicionarPendenciaEcf(PendenciaEcf pendenciaEcf)
+        {
+            PendenciasEcfs.Add(pendenciaEcf);
+        }
+    }
+}
