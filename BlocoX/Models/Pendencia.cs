d265b1fb8d2e6e75f356914c2e58e32e6d24c3f5
Mario felippe <mfchaves@live.com>
2022-02-18 00:36:36 -0200

    Criação da classe Pendencia.

---
+++ BlocoX/Models/Pendencia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class Pendencia
+    {
+        private int codigo;
+        private string descricao;
+        private int quantidade;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public int Quantidade { get => quantidade; set => quantidade = value; }
+    }
+}
