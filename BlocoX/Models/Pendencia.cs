ddfa4178ffc403e0733278aa57e323640dee33c4
Mario felippe <mfchaves@live.com>
2022-02-18 15:53:22 -0200

    Ajuste no construtor da classe.

--- BlocoX/Models/Pendencia.cs
+++ BlocoX/Models/Pendencia.cs
@@ -12,6 +12,13 @@ namespace BlocoX.Models
         private string descricao;
         private int quantidade;
 
+        public Pendencia(int codigo, string descricao, int quantidade)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Quantidade = quantidade;
+        }
+
         public int Codigo { get => codigo; set => codigo = value; }
         public string Descricao { get => descricao; set => descricao = value; }
         public int Quantidade { get => quantidade; set => quantidade = value; }
