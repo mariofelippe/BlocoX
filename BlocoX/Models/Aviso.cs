8472fb58fb83d215a0c2a5f6f8f436a52869feac
Mario felippe <mfchaves@live.com>
2022-02-18 00:37:15 -0200

    Criação da classe Aviso.

---
+++ BlocoX/Models/Aviso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class Aviso
+    {
+        private int codigo;
+        private string descricao;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+    }
+}
