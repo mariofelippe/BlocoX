125f309479d8bd0c3dab7d7757f46443519a576b
Mario Felippe <mfchaves@live.com>
2022-03-03 22:45:12 -0300

    Correção de erro de valores nulos.

--- BlocoX/Models/PendenciaContribuinte.cs
+++ BlocoX/Models/PendenciaContribuinte.cs
@@ -14,7 +14,17 @@ namespace BlocoX.Models
 
         public Estabelecimento Estabelecimento { get => estabelecimento; set => estabelecimento = value; }
         public DateTime DataInicioObrigacao { get => dataInicioObrigacao; set => dataInicioObrigacao = value; }
-        public List<PendenciaEcf> PendenciasEcfs { get => pendenciasEcfs; set => pendenciasEcfs = value; }
+        public List<PendenciaEcf> PendenciasEcfs 
+        {
+            get
+            { 
+                if(pendenciasEcfs == null)
+                {
+                    pendenciasEcfs = new List<PendenciaEcf>();
+                }
+                return pendenciasEcfs;
+            }
+            set => pendenciasEcfs = value; }
 
         public void AdicionarPendenciaEcf(PendenciaEcf pendenciaEcf)
         {
