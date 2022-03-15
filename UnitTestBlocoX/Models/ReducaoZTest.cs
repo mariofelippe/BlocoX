e668c6b8e5c86eb7e6ff59aef3e1b94d013519a5
Mario Felippe <mfchaves@live.com>
2022-03-14 21:00:26 -0300

    Teste da função para ajustar a sequência dos totalizadores.

--- UnitTestBlocoX/Models/ReducaoZTest.cs
+++ UnitTestBlocoX/Models/ReducaoZTest.cs
@@ -64,5 +64,26 @@ namespace UnitTestBlocoX.Models
             reducaZ.AjustarVarlorVendaBrutaDiaria();
             Assert.AreEqual("Reducao Z 01022022", reducaZ.GeraNomeReducaoZ());
         }
+        [TestMethod]
+        public void TestAjustarSequenciaTotalizador()
+        {
+            Estabelecimento estabelecimento = new Estabelecimento("24123456");
+            Ecf ecf = new Ecf("BE111610101110048745");
+            PafEcf paf = new PafEcf("00112300001237");
+            DadosReducao dadosReducao = new DadosReducao(DateTime.Parse("2022-02-01"), DateTime.Now, 2, 4, 1, 10000.54M, 1M);
+            Produto produto = new Produto("Produto Test", "4714247187047", "0", "66019110", "2169997", 50.00M, "PC", 140.10M, 0, 100M, 1140.54M);
+            Produto produto2 = new Produto("Produto Test 2", "7896002304771", "0", "66015510", "4664195", 5.00M, "PC", 10.10M, 0, 1M, 21.44M);
+            
+            TotalizadorParcial totalizador = new TotalizadorParcial("05T1700", 50.00M);
+            totalizador.AdicionarProduto(produto);
+            TotalizadorParcial totalizador2 = new TotalizadorParcial("01T1200", 5.00M);
+            totalizador.AdicionarProduto(produto2);
+            ReducaoZ reducaZ = new ReducaoZ(ecf, dadosReducao);
+            reducaZ.AdicionarTotalizador(totalizador);
+            reducaZ.AdicionarTotalizador(totalizador2);
+            reducaZ.AjustarSequenciaTotalizador();
+            Assert.AreEqual("01T1700", reducaZ.Totalizadores[0].Nome);
+            Assert.AreEqual("02T1200", reducaZ.Totalizadores[1].Nome);
+        }
     }
 }
