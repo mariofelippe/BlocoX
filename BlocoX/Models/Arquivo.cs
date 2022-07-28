7b1035dd66e319d3786aa40bdd8150e67c046bbe
Mario Felippe <mfchaves@live.com>
2022-07-28 10:58:06 -0300

    Criação de classes para função de listar arquivos.

---
+++ BlocoX/Models/Arquivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class Arquivo
+    {
+        public string Recibo { get; set; }
+        public string Ecf { get; set; }
+        public DateTime DataRefenrencia { get; set; }
+        public DateTime DataHoraRecepcao { get; set; }
+        public DateTime DataHoraProcessamento { get; set; }
+        public int TipoRecepcaoCodigo { get; set; }
+        public string TipoRecepcaoDescricao { get; set; }
+        public int SituacaoProcessamentoCodigo { get; set; }
+        public string SituacaoProcessamentoDescricao { get; set; }
+
+        public Arquivo(string recibo, string ecf, DateTime dataRefenrencia, DateTime dataHoraRecepcao, DateTime dataHoraProcessamento, int tipoRecepcaoCodigo, string tipoRecepcaoDescricao, int situacaoProcessamentoCodigo, string situacaoProcessamentoDescricao)
+        {
+            Recibo = recibo;
+            Ecf = ecf;
+            DataRefenrencia = dataRefenrencia;
+            DataHoraRecepcao = dataHoraRecepcao;
+            DataHoraProcessamento = dataHoraProcessamento;
+            TipoRecepcaoCodigo = tipoRecepcaoCodigo;
+            TipoRecepcaoDescricao = tipoRecepcaoDescricao;
+            SituacaoProcessamentoCodigo = situacaoProcessamentoCodigo;
+            SituacaoProcessamentoDescricao = situacaoProcessamentoDescricao;
+        }
+    }
+}
---
+++ BlocoX/Models/ListaArquivo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoX.Models
+{
+    public class ListaArquivo
+    {
+        public int SituacaoOperacaoCodigo { get; set; }
+        public string SituacaoOperacaoDescricao { get; set; }
+        public string Ie { get; set; }
+
+        public List<Arquivo> Arquivos { get; set; } = new List<Arquivo>();
+
+        public void AdicionarArquivo (Arquivo arquivo)
+        {
+            Arquivos.Add(arquivo);
+        }
+    }
+}
